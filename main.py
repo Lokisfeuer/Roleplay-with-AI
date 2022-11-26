dfc3dc16e94eb42817ee943db3461e9633fa2b57
Lokisfeuer <thede.v.oertzen@web.de>
2022-11-26 07:15:31 +0100

    implemented openAI

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import random
-import numpy as np
-import torch
-import torch.nn as nn
+import os
+import openai
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = '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'
+
+
+
 
 def main():
-    print('Hi')
-    print(random.randint(5))
+    response = openai.Completion.create(model="text-davinci-002",
+                                        prompt="Say this is a test",
+                                        temperature=0,
+                                        max_tokens=6)
+    response = response['choices'][0]['text']
+    print(response)
 
 
 if __name__ == '__main__':
